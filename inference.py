4a2637808c1c35082b9471e5d8821ddd0ecd153b
JelsaOverlandFrost <frosty@gtriipglobal.com>
2023-07-24 17:06:11 +0800

    Init update

--- inference.py
+++ inference.py
@@ -7,25 +7,33 @@
 @time: 2020/4/5 9:25
 
 """
+
+import os
+import cv2
+import time
+import torch
+import torch.nn.functional as F
+import numpy as np
+from PIL import Image
+from tqdm import tqdm
+from models.ctpn import *
+from torchvision.transforms import transforms
+from utils.text_connector.detectors import TextDetector
+from utils.rpn_msr.proposal_layer import proposal_layer
+import torch.nn.functional as F
+import torch
+import numpy as np
+import cv2
 import os
 
 os.environ['CUDA_VISIBLE_DEVICES'] = '0'
-import cv2
-import numpy as np
-import torch
-import torch.nn.functional as F
-from utils.rpn_msr.proposal_layer import proposal_layer
-from utils.text_connector.detectors import TextDetector
-from torchvision.transforms import transforms
-from models.ctpn import *
-import time
-from tqdm import tqdm
-from PIL import Image
 
-def Add_Padding(image,top, bottom, left, right, color):
+
+def Add_Padding(image, top, bottom, left, right, color):
     padded_image = cv2.copyMakeBorder(image, top, bottom,
                                       left, right, cv2.BORDER_CONSTANT, value=color)
     return padded_image
+
 
 def rotate(img, angle):
     w, h = img.shape[:2]
@@ -33,8 +41,9 @@ def rotate(img, angle):
     img_rotation = cv2.warpAffine(img, rotation_matrix, (h, w))
     return img_rotation
 
-def resize_image(img,max_size=1200,color=(0,0,0)):
-    
+
+def resize_image(img, max_size=1200, color=(0, 0, 0)):
+
     img_size = img.shape
     im_size_max = np.max(img_size[0:2])
     im_scale = float(max_size) / float(im_size_max)
@@ -45,8 +54,8 @@ def resize_image(img,max_size=1200,color=(0,0,0)):
     new_w_w = new_w if new_w // 16 == 0 else (new_w // 16 + 1) * 16
 
     re_im = cv2.resize(img, (new_w_w, new_h_h), interpolation=cv2.INTER_LINEAR)
-           
-    return re_im, (im_scale*(new_h_h/new_h),im_scale*(new_w_w/new_w))
+
+    return re_im, (im_scale*(new_h_h/new_h), im_scale*(new_w_w/new_w))
 
 
 def toTensorImage(image, is_cuda=True):
@@ -59,10 +68,10 @@ def toTensorImage(image, is_cuda=True):
 
 
 class DetectImg():
-    
-    def load_model(self, model_file,base_model,detect_type):
+
+    def load_model(self, model_file, base_model, detect_type):
         model_dict = torch.load(model_file)
-        model = CTPN_Model(base_model,pretrained=False).cuda()
+        model = CTPN_Model(base_model, pretrained=False).cuda()
         model.load_state_dict(model_dict)
         self.model = model
         self.detect_type = detect_type
@@ -91,8 +100,9 @@ class DetectImg():
         scores = textsegs[:, 0]
         textsegs = textsegs[:, 1:5]
 
-        textdetector = TextDetector(DETECT_MODE = self.detect_type)
-        boxes, text_proposals = textdetector.detect(textsegs, scores[:, np.newaxis], img_ori.shape[:2])
+        textdetector = TextDetector(DETECT_MODE=self.detect_type)
+        boxes, text_proposals = textdetector.detect(
+            textsegs, scores[:, np.newaxis], img_ori.shape[:2])
         boxes = np.array(boxes, dtype=np.int)
         text_proposals = text_proposals.astype(np.int)
         return boxes, text_proposals, rh, rw
@@ -103,36 +113,38 @@ def show_img(save_path, im_file, boxes, text_proposals):
     img_ori, (rh, rw) = resize_image(img_ori)
     im_name = im_file.split('/')[-1].split('.')[0]
     for item in text_proposals:
-        img_ori = cv2.rectangle(img_ori, (item[0], item[1]), (item[2], item[3]), (0, 255, 255))
-    img_ori = cv2.resize(img_ori, None, None, fx=1.0 / rw, fy=1.0 / rh, interpolation=cv2.INTER_LINEAR)
+        img_ori = cv2.rectangle(
+            img_ori, (item[0], item[1]), (item[2], item[3]), (0, 255, 255))
+    img_ori = cv2.resize(img_ori, None, None, fx=1.0 / rw,
+                         fy=1.0 / rh, interpolation=cv2.INTER_LINEAR)
     for i, box in enumerate(boxes):
         cv2.polylines(img_ori, [box[:8].astype(np.int32).reshape((-1, 1, 2))], True, color=(0, 255, 0),
-                      thickness =2)
-    img_ori = cv2.resize(img_ori, None, None, fx=0.9, fy=0.9, interpolation=cv2.INTER_LINEAR)
+                      thickness=2)
+    img_ori = cv2.resize(img_ori, None, None, fx=0.9,
+                         fy=0.9, interpolation=cv2.INTER_LINEAR)
     cv2.imwrite(os.path.join(save_path, im_name + '.jpg'), img_ori)
 
 
 if __name__ == "__main__":
 
-
-#     dir_path = '/src/notebooks/train_data/icdar2015/ch4_test_images'
+    #     dir_path = '/src/notebooks/train_data/icdar2015/ch4_test_images'
     dir_path = '/home/aistudio/work/data/MTWI2018/image'
 
-    
-    model_file = './model_save/ctpn_35.pth'
+    model_file = './model_save/mobilenet_v3_large.pth'
     img_save_path = './result'
     txt_save_path = './pre_gt'
-    detect_type = 'O' # 'O' or 'H'
+    detect_type = 'O'  # 'O' or 'H'
     base_model = 'shufflenet_v2_x1_0'
-    
+
     detect_obj = DetectImg()
-    detect_obj.load_model(model_file,base_model,detect_type)
-    
+    detect_obj.load_model(model_file, base_model, detect_type)
+
     files = os.listdir(dir_path)
     bar = tqdm(total=len(files))
     for file in files:
         bar.update(1)
-        fid = open(os.path.join(txt_save_path, 'res_' + file.split('.')[0] + '.txt'), 'w+', encoding='utf-8')
+        fid = open(os.path.join(txt_save_path, 'res_' +
+                   file.split('.')[0] + '.txt'), 'w+', encoding='utf-8')
         im_file = os.path.join(dir_path, file)
         boxes, text_proposals, rh, rw = detect_obj.detect(im_file)
         for i, box in enumerate(boxes):
@@ -144,6 +156,3 @@ if __name__ == "__main__":
             fid.write(','.join(box) + '\n')
         fid.close()
         show_img(img_save_path, im_file, boxes, text_proposals)
-
-
-
